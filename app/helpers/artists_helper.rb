764da0da551a408e0da3712fbe5b186498139553
Adam Duff <j.adam.duff@gmail.com>
2018-09-27 22:35:46 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,7 +2,9 @@ module ArtistsHelper
 
   def display_artist
     if self.artist_id.present?
-      <%= link_to self.artist.name, artist_path(self.artist) %>
+      link_to self.artist.name, artist_path(self.artist)
+    else
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 
